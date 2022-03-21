fe1aa11f4f0b5c38f62513e10585ee81dfb420cf
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-03-21 17:31:36 -0500

    Otra vez, otro cambio de Sintáxis
    Esto de la sintáxis es bien aburridor aaaaa

--- src/main/java/Motor.java
+++ src/main/java/Motor.java
@@ -9,8 +9,10 @@ public class Motor {
 
     void asignarTipo(String tipo){
         switch (tipo){
-            case "electrico", "gasolina" -> {this.tipo = tipo;}
-            default -> {}
+            case "electrico":
+            case "gasolina":
+                this.tipo = tipo;
+            default:
         }
     }
 }
